55359fd4c4ae2db40af0a537a88abea9eb2e833c
NicolasRoberts1 <roberts.nicolas.alejandro@gmail.com>
2024-08-23 18:51:44 -0300

    Creacion de controladores (base)

--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -1,15 +1,18 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Pedidos') }}
+        </h2>
+    </x-slot>
 
-        <title>Pedidos</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-
-    </head>
-    <body class="antialiased">
-
-    </body>
-</html>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
 
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('orders');
+})->middleware(['auth', 'verified'])->name('orders');;
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/orders', function () {
+        return view('orders');
+    })->name('orders');
 });
 
 require __DIR__.'/auth.php';
